4423bc2e762b589e031c5ad9d84858f5ac4353ad
arleenmonteiro <monteiak@mail.uc.edu>
2022-09-18 20:42:56 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int numToExamine=2;
+        double mod=numToExamine%2;
+        if(mod == 0.0) {
+            System.out.println("The number " + numToExamine + " is even.");
+        }
+        else{
+            System.out.println("The number " + numToExamine + " is odd.");
+        }
+
+
+
+    }
+}
+
